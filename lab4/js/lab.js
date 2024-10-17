b2ba30c932cfaca9f98634bf97da53d4b2692855
lylliantimothee <lylliantimothee1423@gmail.com>
2024-10-16 22:37:00 -0700

    plplpl

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Hangman Implementation
+// Author: Lyllian Timothee
+// Date: 10/16/24
 
 // Constants
 
@@ -19,3 +19,16 @@ function main() {
 
 // let's get this party started
 main();
+
+// SIMPLE COMPUTER GAME OUTLINE:
+// Creating a Hangman game:
+//    Generate a word at random
+//    For each letter, print an underscore
+//      Hide letters from user
+//    Print post for hanging figure
+//    Print English alphabet for user
+//    User provides input by clicking letters from the alphabet
+//      If input is incorrect--not a letter or in the word--print figure part "n"
+//      If input is correct, print all letters that are the same as the input
+//    When all letters in the word are validated, flash victory screen
+//      If "n" equals 6, flash lose screen
