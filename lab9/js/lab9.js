cfec650e821bc848e5047c28024e9e1e48029036
lylliantimothee <lylliantimothee1423@gmail.com>
2024-11-04 11:16:22 -0800

    sdfjkfjd

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -1,6 +1,6 @@
 // index.js - purpose and description here
 // Author: Lyllian Timothee
-// Date: 
+// Date: 11/4/2024
 
 // Constants
 
@@ -11,6 +11,34 @@ function myFunction(param1, param2) {
   // some code here
   // return results;
 }
+
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+$("#button-challenge").click(function() {
+  $("#challenge").toggleClass("special");
+  $("#button-challenge").toggleClass("normal")
+
+});
+
+$("#problems").append("<button id='button-problems'>Make Special</button>");
+$("#button-problems").click(function() {
+  $("#problems").toggleClass("special");
+  $("#button-problems").toggleClass("normal");
+
+});
+
+$("#reflection").append("<button id='button-reflection'>Make Special</button>");
+$("#button-reflection").click(function() {
+  $("#reflection").toggleClass("special");
+  $("#button-reflection").toggleClass("normal");
+
+});
+
+$("#results").append("<button id='button-results'>Make Special</button>");
+$("#button-results").click(function() {
+  $("#results").toggleClass("special");
+  $("#button-results").toggleClass("normal");
+
+});
 
 function defaultName() {
 
