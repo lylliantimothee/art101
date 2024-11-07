baa67aa0f8ca1ff6f72a3168b326088b0e3678ab
lylliantimothee <lylliantimothee1423@gmail.com>
2024-11-06 18:04:37 -0800

    wwef

--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -1,6 +1,6 @@
 // index.js - purpose and description here
 // Author: Lyllian Timothee
-// Date: 
+// Date: 11/6/2024
 
 // Constants
 
@@ -12,9 +12,21 @@ function myFunction(param1, param2) {
   // return results;
 }
 
-function defaultName() {
 
+function generateRandomText() {
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  return text.slice(randStart, randStart + randLen);
 }
+
+$("#make-convo").click(function() {
+  const newText = generateRandomText();
+  $("#output").append('<div class=text><p>' + newText + '</p></div>');
+})
+
 
 function main() {
   console.log("Main function started.");
