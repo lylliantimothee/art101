e9ee5289dbc705a0bff9342149f0cc79a4653f32
lylliantimothee <lylliantimothee1423@gmail.com>
2024-11-13 19:41:08 -0800

    adsfjkjafn

--- lab12/js/lab12.js
+++ lab12/js/lab12.js
@@ -1,6 +1,6 @@
 // index.js - purpose and description here
 // Author: Lyllian Timothee
-// Date: 
+// Date: 11/13/2024
 
 // Constants
 
@@ -12,9 +12,28 @@ function myFunction(param1, param2) {
   // return results;
 }
 
-function defaultName() {
-
+function sortingHat(str) {
+  length = str.length;
+  mod = length % 4;
+  if (mod == 0 ) {
+    return "Gryffindor";
+  }
+  else if (mod == 1 ) {
+    return "Ravenclaw";
+  }
+  else if (mod == 2 ) {
+    return "Slytherin";
+  }
+  else if (mod == 3 ) {
+    return "Hufflepuff";
+  }
 }
+
+$("#button").click(function() {
+  let name = $("#input").val();
+  let house = sortingHat(name);
+  $("#output").html('<div id="thing"><p>The Sorting Hat has sorted you into ' + house + '!</p></div>');
+});
 
 function main() {
   console.log("Main function started.");
