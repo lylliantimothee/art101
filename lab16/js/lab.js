0b1266e29b1b729bf6128d3442670b2056e31f55
lylliantimothee <lylliantimothee1423@gmail.com>
2024-12-02 22:05:31 -0800

    sdfsfdf

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -13,43 +13,50 @@ function myFunction(param1, param2) {
   // return results;
 }
 
+let endpoint = "https://stand-by-me.herokuapp.com/api/v1/stands/1"
 
-$.ajax ({
-                // The URL for the request (from the api docs)
-                url: "*https://xkcd.com/614/info.0.json",
-                // The data to send (will be converted to a query string)
-                comicObj: {
-                              "month": "7",
-                              "num": 614,
-                              "link": "",
-                              "year": "2009",
-                              "news": "",
-                              "safe_title": "Woodpecker",
-                              "transcript": "[[A man with a beret and a woman are standing on a boardwalk, leaning on a handrail.]]\nMan: A woodpecker!\n<<Pop pop pop>>\nWoman: Yup.\n\n[[The woodpecker is banging its head against a tree.]]\nWoman: He hatched about this time last year.\n<<Pop pop pop pop>>\n\n[[The woman walks away.  The man is still standing at the handrail.]]\n\nMan: ... woodpecker?\nMan: It's your birthday!\n\nMan: Did you know?\n\nMan: Did... did nobody tell you?\n\n[[The man stands, looking.]]\n\n[[The man walks away.]]\n\n[[There is a tree.]]\n\n[[The man approaches the tree with a present in a box, tied up with ribbon.]]\n\n[[The man sets the present down at the base of the tree and looks up.]]\n\n[[The man walks away.]]\n\n[[The present is sitting at the bottom of the tree.]]\n\n[[The woodpecker looks down at the present.]]\n\n[[The woodpecker sits on the present.]]\n\n[[The woodpecker pulls on the ribbon tying the present closed.]]\n\n((full width panel))\n[[The woodpecker is flying, with an electric drill dangling from its feet, held by the cord.]]\n\n{{Title text: If you don't have an extension cord I can get that too.  Because we're friends!  Right?}}",
-                              "alt": "If you don't have an extension cord I can get that too.  Because we're friends!  Right?",
-                              "img": "https://imgs.xkcd.com/comics/woodpecker.png",
-                              "title": "Woodpecker",
-                              "day": "24"
-                          },
-                // Whether this is a POST or GET request
-                type: "GET",
-                // The type of data we expect back
-                dataType : "json",
-                // What do we do when the api call is successful
-                //   all the action goes in here
-                success: function(comicObj) {
-                    // do stuff
-                    console.log(comicObj);
-                    $("#output").append("<h3>" + comicObj.title + "</h3>" + "<img>" + comicObj.img + "</img>" + comicObj.alt);
-                },
-                // What we do if the api call fails
-                error: function (jqXHR, textStatus, errorThrown) { 
-                    // do stuff
-                    console.log("Error:", textStatus, errorThrown);
-                }
-        })
+let ajaxConfig = {
+                    // The URL for the request (from the api docs)
+                    url: endpoint,
+                    // The data to send (will be converted to a query string)
+                    comicObj: {
+                                  "id": "1",
+                                  "name": "The World",
+                                  "alternateName": "none",
+                                  "japaneseName": "ザ・ワールド(世界)",
+                                  "image": "theworld.png",
+                                  "standUser": "2",
+                                  "chapter": "Stardust Crusaders, Steel Ball Run",
+                                  "abilities": "Time Stop, Super Speed, Super Strength",
+                                  "battlecry": "MUDAMUDAMUDA"
+                              },
+                    // Whether this is a POST or GET request
+                    type: "GET",
+                    // The type of data we expect back
+                    dataType : "json",
+                    // What do we do when the api call is successful
+                    //   all the action goes in here
+                    success: function(comicObj) {
+                        // do stuff
+                        console.log(comicObj);
+                        $("#output").append("<h3>Stands</h3>");
+                        $("#output").append("<p>Number: " + comicObj.id + "</p>");
+                        $("#output").append("<p>Name: " + comicObj.name + "</p>");
+                        $("#output").append("<p>Alternate Name: " + comicObj.alternateName + "</p>");
+                        $("#output").append("<p>Japanese Name: " + comicObj.japaneseName + "</p>");
+                        $("#output").append("<p>Stand User: " + comicObj.standUser + "</p>");
+                        $("#output").append("<p>Chapter: " + comicObj.chapter + "</p>");
+                        $("#output").append("<p>Abilities: " + comicObj.abilities + "</p>");
+                        $("#output").append("<p>Battle Cry!: " + comicObj.battlecry + " </p>");
+                    },
+                    // What we do if the api call fails
+                    error: function (jqXHR, textStatus, errorThrown) { 
+                        // do stuff
+                        console.log("Error:", textStatus, errorThrown);
+                    }
+                  }
 
-
+$.ajax(ajaxConfig);
 
 function main() {
   console.log("Main function started.");
